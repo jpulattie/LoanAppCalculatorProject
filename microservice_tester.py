4d31788453edb3ae6624d3596e634768cacf490f
jpulattie <jpulattie@gmail.com>
2024-02-25 16:27:23 -0600

    Initial repo creation

---
+++ microservice_tester.py
@@ -0,0 +1,21 @@
+
+import zmq
+
+context = zmq.Context()
+
+#  Socket to talk to server
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+def request(toop):
+    toop_bytes =  str(toop).encode()
+    #print(toop_bytes)
+
+    socket.send(toop_bytes)
+
+    #  Get the reply.
+    message = socket.recv()
+    decoded = message.decode()
+    #print("Received reply %s [ %s ]" % (toop_bytes, decoded))
+    return decoded
+print(request((1,"BOTW")))
