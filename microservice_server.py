71e37f06dd89db7e12f3e2c58ce68238c43e4120
jpulattie <97932025+jpulattie@users.noreply.github.com>
2024-02-25 16:26:27 -0600

    Add files via upload

---
+++ microservice_server.py
@@ -0,0 +1,36 @@
+import random
+import zmq
+
+def microservice(toop=None):
+    global random_num
+    if toop is None:
+        random_num = 0
+        return random_num
+    else:
+        if toop[0] == 1:
+            random_num = random.randrange(1, 389)
+            print(random_num)
+        elif toop[1] == 2:
+            random_num = random.randrange(1, 509)
+        else:
+            random_num = "Invalid input. Please try again."
+        return random_num
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+while True:
+    message = socket.recv()
+    print("Received request: %s" % message)
+    decoded = message.decode()
+    toop = eval(decoded)
+
+    print(toop)
+    print(type(toop))
+    print(toop[0])
+    message = microservice(toop)
+    random_num_encoded = str(message).encode()
+
+    socket.send(random_num_encoded)
+
